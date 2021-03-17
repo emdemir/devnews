85d444e6b8aa3bfe5fa644b7aaba40808ee91e9b
Efe Mert Demir <emdemir@users.noreply.github.com>
2021-03-17 02:02:25 +0000

    web: Implement user details page

--- app/src/web/routes/user.ts
+++ app/src/web/routes/user.ts
@@ -1,12 +1,38 @@
 import Router = require("koa-router");
 
 import type { AppContext } from "../";
+import type UserManager from "../../base/user_manager";
 
-export default function({ }) {
+interface Dependencies {
+    userManager: UserManager;
+};
+
+export default function({ userManager }: Dependencies) {
     const router = new Router<any, AppContext>();
 
-    router.get("/", async ctx => {
-        ctx.body = "TEST";
+    router.get("/:username/", async ctx => {
+        const { user } = ctx.state;
+        const { username } = ctx.params;
+
+        // Get the subject user
+        const subject = await userManager.getUserByUsername(username, {
+            commentCount: true,
+            storyCount: true,
+            commentKarma: true,
+            storyKarma: true,
+        });
+
+        if (subject === null) {
+            // Show 404 to the user
+            ctx.status = 404;
+            return await ctx.render("pages/404.html", {
+                reason: "nouser", user
+            });
+        }
+
+        return ctx.render("pages/user.html", {
+            subject, user
+        })
     });
 
     return router;
