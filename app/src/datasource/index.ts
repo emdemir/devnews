de974d8027d9ec9f93155cc4238107bbcf1eb21e
Efe Mert Demir <emdemir@users.noreply.github.com>
2021-03-17 01:28:22 +0000

    Accept DEBUG_QUERIES envvar to dump queries to stdout

--- app/src/datasource/index.ts
+++ app/src/datasource/index.ts
@@ -1,4 +1,7 @@
 import { Pool, PoolClient, QueryResult } from "pg";
+import debugFactory = require("debug");
+
+const debug = debugFactory("devnews:datasource");
 
 // The connection information is gathered from environment variables described in
 // docker-compose.yml. For more information, see:
@@ -16,6 +19,9 @@ export function query<T>(text: string, params: any): Promise<QueryResult<T>>;
 export function query<T>(text: string, params: any, callback: QueryCallback<T>): void;
 // Definition
 export function query<T = any>(text: string, params: any, callback?: QueryCallback<T>) {
+    if (process.env.DEBUG_QUERIES)
+        debug("executing query:", text.replace(/\s+/g, " "), "params:", params);
+
     if (callback) {
         return pool.query(text, params, callback);
     } else {
