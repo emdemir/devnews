f595a33119a8fa36e18540fc0a41a8a168b426fe
Efe Mert Demir <emdemir@users.noreply.github.com>
2021-03-22 11:29:38 +0000

    base: Add status codes to exceptions
    This prevents the need to catch them in the presentation layer and
    throw the same response code over and over again.

--- app/src/base/exceptions.ts
+++ app/src/base/exceptions.ts
@@ -2,11 +2,15 @@
  * Error used to signal that the user tried to access a document they don't have
  * permissions for.
  */
-export class ForbiddenError extends Error { }
+export class ForbiddenError extends Error {
+    public status = 403;
+}
 /**
  * Error used to signal that an object does not exist.
  */
-export class NotFoundError extends Error { }
+export class NotFoundError extends Error {
+    public status = 404;
+}
 
 /**
  * Type used to carry validation errors in the form of an array of strings.
